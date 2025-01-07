4bc2dc13979b7b4b617c630e7910571a5460bcf3
Brian <brian950804@gmail.com>
2025-01-07 21:21:28 +0800

    Adjust the fourier series code.

--- fourier_transform.py
+++ fourier_transform.py
@@ -10,7 +10,7 @@ def fourier_transform(order_list, num_of_coef):
     
     num_of_point = len(order_list)
 
-    if num_of_coef % 2 == 0:
+    if num_of_coef % 2 == 1:
         num_of_coef += 1
 
     t_list1 = np.linspace(0, 1, num_of_point, endpoint=False)    # 計算係數的黎曼和陣列
@@ -20,9 +20,9 @@ def fourier_transform(order_list, num_of_coef):
     coef_list = []
     
     # 利用黎曼和計算傅立葉級數的係數
-    for n in range(-num_of_coef//2+1, num_of_coef//2+1):
+    for n in range(-num_of_coef//2, num_of_coef//2+1):
         coordinate = order_list[:, 0] + 1j * order_list[:, 1]
-        coef = np.sum(coordinate * (np.exp((1j) * (-2 * pi * n * t_list1)) * dt))
+        coef = np.sum(coordinate * (np.exp((1j) * (-2*pi*n*t_list1)) * dt))
         coef_list.append(coef)
     
 
@@ -30,7 +30,7 @@ def fourier_transform(order_list, num_of_coef):
     point_list = np.zeros(len(t_list2), dtype=complex)
 
     for n in range(-num_of_coef//2+1, num_of_coef//2+1):
-        point_list += coef_list[n+num_of_coef//2] * np.exp((1j) * (2*pi*n*t_list2))
+        point_list += coef_list[n + num_of_coef//2] * np.exp((1j) * (2*pi*n*t_list2))
 
     return point_list 
 
